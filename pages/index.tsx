8c75e58805fe38a4ba0004779c82c92ef4728f1d
redips <redips@prismweaver.com>
2022-05-26 23:35:28 +0900

    [4.3]

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
     return (
-        <div className={"bg-slate-400 py-20 px-10 grid gap-10"}>
-            <div className={"bg-white p-6 rounded-2xl rounded-3xl shadow-2xl"}>
+        <div className={"bg-slate-400 py-20 px-20 grid gap-10"}>
+            <div className={"bg-white p-6 rounded-3xl shadow-2xl"}>
                 <span className={"font-semibold text-3xl"}>Select Item</span>
                 <div className={"flex justify-between my-2"}>
                     <span className={"text-gray-500"}>Gery chair</span>
@@ -19,9 +19,30 @@ const Home: NextPage = () => {
                 </div>
                 <div className={"mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto"}>Checkout</div>
             </div>
-            <div className={"bg-white p-10 rounded-2xl shadow-2xl"}></div>
-            <div className={"bg-white p-10 rounded-2xl shadow-2xl"}></div>
-            <div className={"bg-white p-10 rounded-2xl shadow-2xl"}></div>
+            <div className={"bg-white overflow-hidden rounded-3xl shadow-2xl"}>
+                <div className={"bg-blue-500 p-6 pb-14"}>
+                    <span className={"text-white text-2xl"}>Profile</span>
+                </div>
+                <div className={"rounded-3xl relative -top-5 bg-white p-6"}>
+                    <div className={"flex justify-between items-end relative -top-16"}>
+                        <div className={"flex flex-col items-center"}>
+                            <span className={"text-sm text-gray-500"}>Order</span>
+                            <span className={"font-medium"}>340</span>
+                        </div>
+                        <div className={"h-24 w-24 bg-red-400 rounded-full"}></div>
+                        <div className={"flex flex-col items-center"}>
+                            <span className={"text-sm text-gray-500"}>Spent</span>
+                            <span className={"font-medium"}>$2,310</span>
+                        </div>
+                    </div>
+                    <div className={"relative flex flex-col items-center -mt-10 -mb-5"}>
+                        <span className={"text-lg font-medium"}>Tony Molloy</span>
+                        <span className={"text-sm text-gray-500"}>미국</span>
+                    </div>
+                </div>
+            </div>
+            <div className={"bg-white p-6 rounded-3xl shadow-2xl"}></div>
+            <div className={"bg-white p-6 rounded-3xl shadow-2xl"}></div>
         </div>
     );
 }
