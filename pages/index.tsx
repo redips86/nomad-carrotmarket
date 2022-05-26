68366fef0c91de0bc4c176a4c0a030a0b3893763
redips <redips@prismweaver.com>
2022-05-27 07:14:03 +0900

    [4.8] file

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@ import type {NextPage} from 'next'
 const Home: NextPage = () => {
     return (
         <div className={"flex flex-col space-y-2 p-5"}>
-            <details className={"select-none open:text-white open:bg-indigo-400"}>
-                <summary className={"cursor-pointer"}>What is my fav. food.</summary>
-                <span>김치</span>
-            </details>
+            <input type="file" className="file:cursor-pointer file:transition-colors
+            file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border
+            file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"/>
         </div>
     );
 }
