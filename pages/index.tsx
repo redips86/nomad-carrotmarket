79fd31f166f2c9a5cd73cadc5145a1b8a7ba2c3f
redips <redips@prismweaver.com>
2022-05-27 06:25:43 +0900

    [4.6]

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,23 @@ const Home: NextPage = () => {
         <div className={"bg-slate-400 py-20 px-20 grid gap-10 min-h-screen"}>
             <div className={"bg-white p-6 rounded-3xl shadow-2xl"}>
                 <span className={"font-semibold text-3xl"}>Select Item</span>
-                <div className={"flex justify-between my-2"}>
-                    <span className={"text-gray-500"}>Gery chair</span>
-                    <span className={"font-semibold"}>$19</span>
-                </div>
-                <div className={"flex justify-between"}>
-                    <span className={"text-gray-500"}>Gery chair</span>
-                    <span className={"font-semibold"}>$19</span>
-                </div>
+                <ul>
+                    {
+                        [1, 2, 3, 4, 5].map((i) =>
+                            <div key={i} className="flex justify-between my-2
+                            odd:bg-blue-50 even:bg-yellow-500">
+                                <span className={"text-gray-500"}>Gery chair</span>
+                                <span className={"font-semibold"}>$19</span>
+                            </div>)
+                    }
+                </ul>
+                <ul>
+                    {
+                        ["a", "b", "c", ""].map((c, i) => {
+                            return <li key={i} className={"bg-red-500 py-2 empty:hidden"}>{c}</li>
+                        })
+                    }
+                </ul>
                 <div className={"flex justify-between mt-2 pt-2 border-t-2 border-dashed"}>
                     <span>Total</span>
                     <span className={"font-semibold"}>$10</span>
