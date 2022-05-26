7a7bbcd7083a8e5c0095cc003d8e3d629988f9e6
redips <redips@prismweaver.com>
2022-05-27 07:15:53 +0900

    [4.8] first-letter selector

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,10 @@ import type {NextPage} from 'next'
 const Home: NextPage = () => {
     return (
         <div className={"flex flex-col space-y-2 p-5"}>
-            <input type="file" className="file:cursor-pointer file:transition-colors
-            file:hover:text-purple-400 file:hover:bg-white file:hover:border-purple-400 file:hover:border
-            file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"/>
+            <p className={"first-letter:text-7xl first-letter:hover:text-purple-500"}>Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit. Commodi
+                delectus dignissimos enim est facere facilis ipsam quis tempore veniam! Culpa distinctio enim eveniet
+                exercitationem incidunt inventore laborum officia quod tempore?</p>
         </div>
     );
 }
