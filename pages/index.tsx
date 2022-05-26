07c5780fb4ae9dca7d64a7b76b578317e94d739c
redips <redips@prismweaver.com>
2022-05-27 07:07:11 +0900

    [4.8] detail tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
     return (
-        <form action="" className="flex flex-col space-y-2 p-5">
-            <input type="text"
-                   required
-                   placeholder={"Username"}
-                   className="border p-1 peer border-gray-400 rounded-xl"/>
-            <span className="hidden peer-invalid:block peer-invalid:text-red-500">This input is invalid</span>
-            <span className="hidden peer-valid:block peer-valid:text-teal-500">Awesome username</span>
-            <span className="hidden peer-hover:block peer-hover:text-yellow-500">Hello</span>
-            <input type="submit" value={"Login"} className={"bg-white"}/>
-        </form>
+        <div className={"flex flex-col space-y-2 p-5"}>
+            <details className={"select-none open:text-white open:bg-indigo-400"}>
+                <summary className={"cursor-pointer"}>What is my fav. food.</summary>
+                <span>김치</span>
+            </details>
+        </div>
     );
 }
 
