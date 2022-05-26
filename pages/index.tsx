bf36de108d6f197174839694d408bec7eb0433cb
redips <redips@prismweaver.com>
2022-05-27 05:41:48 +0900

    [4.4]

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,10 @@ const Home: NextPage = () => {
                     <span>Total</span>
                     <span className={"font-semibold"}>$10</span>
                 </div>
-                <div className={"mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto"}>Checkout</div>
+                <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto
+                hover:bg-teal-500 hover:text-black block
+                active:bg-yellow-500 focus:text-red-500">Checkout
+                </button>
             </div>
             <div className={"bg-white overflow-hidden rounded-3xl shadow-2xl"}>
                 <div className={"bg-blue-500 p-6 pb-14"}>
@@ -54,10 +57,10 @@ const Home: NextPage = () => {
                     <span className={"font-medium text-xl"}>Swoon Lounge</span>
                     <span className={"text-xs text-gray-500"}>Chair</span>
                     <div className={"mt-3 mb-5 flex justify-between items-center"}>
-                        <div>
-                            <input type="radio"/>
-                            <input type="radio"/>
-                            <input type="radio"/>
+                        <div className={"space-x-3"}>
+                            <button className={"w-5 h-5 rounded-full bg-yellow-500"}></button>
+                            <button className={"w-5 h-5 rounded-full bg-indigo-500"}></button>
+                            <button className={"w-5 h-5 rounded-full bg-teal-500"}></button>
                         </div>
                         <div className={"flex items-center space-x-5"}>
                             <button
