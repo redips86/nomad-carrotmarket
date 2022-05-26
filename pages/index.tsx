b9b4326a071433691c9ee27d329d500bfdb350d0
redips <redips@prismweaver.com>
2022-05-27 06:07:07 +0900

    [4.5]

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,9 +58,12 @@ const Home: NextPage = () => {
                     <span className={"text-xs text-gray-500"}>Chair</span>
                     <div className={"mt-3 mb-5 flex justify-between items-center"}>
                         <div className={"space-x-3"}>
-                            <button className={"w-5 h-5 rounded-full bg-yellow-500"}></button>
-                            <button className={"w-5 h-5 rounded-full bg-indigo-500"}></button>
-                            <button className={"w-5 h-5 rounded-full bg-teal-500"}></button>
+                            <button
+                                className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
+                            <button
+                                className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
+                            <button
+                                className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
                         </div>
                         <div className={"flex items-center space-x-5"}>
                             <button
