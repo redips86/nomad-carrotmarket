d91522075904c36f810555ae108104f3efd02287
redips <redips@prismweaver.com>
2022-05-29 23:29:29 +0900

    [9.1] Accounts logic

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -1,13 +1,31 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 
 async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const {phone, email} = req.body;
+    const payload = phone ? {phone: +phone} : {email}
+
+    const user = await client.user.upsert({
+        where: {
+            ...payload
+        },
+        create: {
+            name: "Anonymous",
+            ...payload
+        },
+        update: {}
+    })
+
+    console.log(user)
+
     console.log(req.body);
     res.status(200).json({ok: true});
-};
+}
+;
 
 export default withHandler("POST", handler)
