d14b9707c10d6747db53bfb20ea6b462add42592
redips <redips@prismweaver.com>
2022-05-30 23:33:11 +0900

    [11.9] unbound mutation 사용하려고 할 때 되면 바로 떠오르진 않을것 같다. 연습 필요.

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,11 +2,12 @@ import type {NextPage} from "next";
 import Button from "../../components/button";
 import Layout from "../../components/layout";
 import {useRouter} from "next/router";
-import useSWR from "swr";
+import useSWR, {useSWRConfig} from "swr";
 import Link from "next/link";
 import {Product, User} from "@prisma/client";
 import useMutation from "@libs/client/useMutation";
 import {cls} from "@libs/client/utils";
+import useUser from "@libs/client/useUser";
 
 interface ProductWithUser extends Product {
     user: User;
@@ -20,18 +21,23 @@ interface ItemDetailResponse {
 }
 
 const ItemDetail: NextPage = () => {
+    const {user, isLoading} = useUser();
     const router = useRouter();
-    const {data, mutate} = useSWR<ItemDetailResponse>(router.query.id && `/api/products/${router.query.id}`);
+    const {mutate} = useSWRConfig();
+    const {
+        data,
+        mutate: boundMutate
+    } = useSWR<ItemDetailResponse>(router.query.id && `/api/products/${router.query.id}`);
 
     const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
     const onFavClick = () => {
-        toggleFav({});
         if (!data) return;
-        mutate({
+        boundMutate((prev) => prev && {
             ...data,
             isLiked: !data.isLiked
         }, false)
-
+        //mutate("/api/users/me", (prev:any) => ({ok: !prev.ok}), false);
+        toggleFav({});
     };
 
     return (
