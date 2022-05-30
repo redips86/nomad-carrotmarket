1d8ea56aebf18616b49c6aa9830e52523abbcab3
redips <redips@prismweaver.com>
2022-05-30 11:21:07 +0900

    [9.7] Serverless session

--- pages/api/users/confirm.ts
+++ pages/api/users/confirm.ts
@@ -1,17 +1,35 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import withHandler, {ResponseType} from "@libs/server/withHandler";
-
+import {withIronSessionApiRoute} from "iron-session/next";
+import client from "@libs/server/client";
 
 async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseType>
 ) {
     const {token} = req.body;
+    const exists = await client.token.findUnique({
+        where: {
+            payload: token,
+        },
+        // include: {
+        //     user: true
+        // }
+    })
 
-    console.log(token)
+    if (!exists) res.status(404).end();
 
+    console.log(exists);
+    req.session.user = {
+        id: exists?.id
+    }
+
+    await req.session.save();
     res.status(200).end();
 }
 ;
 
-export default withHandler("POST", handler)
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+    cookieName: "carrotsession",
+    password: "2342342423432432432423432098hfay8s90dhrny09sadrvf89yn790nhy3790ahyrnf7094",
+});
