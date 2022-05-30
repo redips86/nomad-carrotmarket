ff4d93429a5640701e2494e620b56d9dc842627d
redips <redips@prismweaver.com>
2022-05-30 15:45:01 +0900

    [11.0] Product model

--- components/item.tsx
+++ components/item.tsx
@@ -16,17 +16,17 @@ export default function Item({
                                id,
                              }: ItemProps) {
   return (
-      <Link href={`/items/${id}`}>
-        <a className="flex px-4 pt-5 cursor-pointer justify-between">
-          <div className="flex space-x-4">
-            <div className="w-20 h-20 bg-gray-400 rounded-md"/>
-            <div className="pt-2 flex flex-col">
-              <h3 className="text-sm font-medium text-gray-900">{title}</h3>
-              <span className="font-medium mt-1 text-gray-900">${price}</span>
-            </div>
-          </div>
-          <div className="flex space-x-2 items-end justify-end">
-            <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
+      <Link href={`/products/${id}`}>
+          <a className="flex px-4 pt-5 cursor-pointer justify-between">
+              <div className="flex space-x-4">
+                  <div className="w-20 h-20 bg-gray-400 rounded-md"/>
+                  <div className="pt-2 flex flex-col">
+                      <h3 className="text-sm font-medium text-gray-900">{title}</h3>
+                      <span className="font-medium mt-1 text-gray-900">${price}</span>
+                  </div>
+              </div>
+              <div className="flex space-x-2 items-end justify-end">
+                  <div className="flex space-x-0.5 items-center text-sm  text-gray-600">
               <svg
                   className="w-4 h-4"
                   fill="none"
--- pages/index.tsx
+++ pages/index.tsx
@@ -22,17 +22,17 @@ const Home: NextPage = () => {
                   hearts={1}
               />
           ))}
-          <FloatingButton href="/items/upload">
-            <svg
-                className="h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-            >
-              <path
-                  strokeLinecap="round"
+          <FloatingButton href="/products/upload">
+              <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+              >
+                  <path
+                      strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M12 6v6m0 0v6m0-6h6m-6 0H6"
