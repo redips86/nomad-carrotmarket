c3e9b1ddbbc9508319d2f456276d809d42cb5a72
redips <redips@prismweaver.com>
2022-05-30 15:55:08 +0900

    [11.1] upload form

--- components/input.tsx
+++ components/input.tsx
@@ -47,6 +47,7 @@ export default function Input({
                         required={required}
                         {...register}
                         type={type}
+                        placeholder="0.00"
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
