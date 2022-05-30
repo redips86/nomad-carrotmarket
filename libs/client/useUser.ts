7fdd24e0d61e037b88cf9fe3b2a8120dd1f767ee
redips <redips@prismweaver.com>
2022-05-30 15:07:32 +0900

    [10.3] npm i swr SWR 다른 탭에서 다시 넘어왔을 떄 재요청하는 기능이 아주 인상적이였다.

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,19 +1,12 @@
-import {useEffect, useState} from "react";
 import {useRouter} from "next/router";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then(response => response.json());
 
 export default function useUser() {
-    const [user, setUser] = useState();
-    const router = useRouter();
-    useEffect(() => {
-        fetch("/api/users/me")
-            .then(response => response.json())
-            .then(data => {
-                if (!data.ok) {
-                    return router.replace("/enter");
-                }
-                setUser(data.profile);
-            });
-    }, [router]);
+    const {data, error} = useSWR("/api/users/me", fetcher);
 
-    return user;
+    const router = useRouter();
+    //return router.replace("/enter");
+    return data;
 }
