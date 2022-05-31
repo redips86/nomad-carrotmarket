5a6bae67b77d64aa9b8f56c0bb81a8f61dc6a69e
redips <redips@prismweaver.com>
2022-05-31 09:47:17 +0900

    [12.2] Post Detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,51 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import withHandler, {ResponseType} from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import {withApiSession} from "@libs/server/withSession";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+    const {query: {id}} = req;
+
+    const post = await client.post.findUnique({
+        where: {
+            id: +id
+        },
+        include: {
+            user: {
+                select: {
+                    id: true,
+                    name: true,
+                    avatar: true
+                }
+            },
+            answers: {
+                select: {
+                    answer: true,
+                    id: true,
+                    user: {
+                        select: {
+                            id: true, name: true, avatar: true
+                        }
+                    }
+                }
+            },
+            _count: {
+                select: {
+                    answers: true,
+                    wonderings: true
+                }
+            }
+        }
+    });
+
+    res.json({ok: true, post})
+}
+
+
+export default withApiSession(withHandler({
+    method: ["GET"],
+    handler
+}));
