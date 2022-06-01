a73117267d6a71f020c85df0c3a38c9583460018
redips <redips@prismweaver.com>
2022-06-01 22:08:17 +0900

    [19.4] _document and Fonts font 최적화

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import Document, {Head, Html, Main, NextScript} from "next/document";
 class CustomDocument extends Document {
     render(): JSX.Element {
         return (<Html lang="ko">
-            <Head/>
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <body>
             <Main/>
             <NextScript/>
