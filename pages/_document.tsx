c1c75ee754323f563cd18a2fd080632757049f9e
redips <redips@prismweaver.com>
2022-06-01 22:07:48 +0900

    [19.4] _document and Fonts font 최적화

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import Document, {Head, Html, Main, NextScript} from "next/document";
+
+class CustomDocument extends Document {
+    render(): JSX.Element {
+        return (<Html lang="ko">
+            <Head/>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>);
+    }
+}
+
+export default CustomDocument;
