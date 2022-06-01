56c7864bb8fa422b801e761a8edb7926116b6535
redips <redips@prismweaver.com>
2022-06-02 00:05:59 +0900

    [19.12] getStaticPaths2 🤔🤔🤔

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,11 +1,27 @@
 import {NextPage} from "next";
+import {readdirSync} from "fs";
 
 const Post: NextPage = () => {
     return <h1>hi</h1>
 }
 
-export function getStaticProps() {
+export function getStaticPaths() {
+    const files = readdirSync("./posts").map(file => {
+        const [name, extension] = file.split(".");
+        return {params: {slug: name}};
+    });
 
+    return {
+        paths: files,
+        fallback: false
+    }
+}
+
+
+export function getStaticProps() {
+    return {
+        props: {},
+    }
 }
 
 export default Post;
