333f431e7ca834a78c319a44cbc51190ab125a85
redips <redips@prismweaver.com>
2022-06-01 17:39:15 +0900

    [15.4] Direct Upload URL

--- pages/api/files.ts
+++ pages/api/files.ts
@@ -6,11 +6,22 @@ async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseType>
 ) {
-    res.json({ok: true, url: ""})
+    const response = await (await fetch(`https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ID}/images/v2/direct_upload`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.CF_TOKEN}`
+        }
+    })).json();
+
+    res.json({
+        ok: true,
+        ...response.result
+    })
 }
 
 
 export default withApiSession(withHandler({
-    method: ["POST"],
+    method: ["GET"],
     handler
 }));
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -39,20 +39,34 @@ const EditProfile: NextPage = () => {
     }, [user]);
     const [editProfile, {data, loading}] = useMutation<EditProfileResponse>(`/api/users/me`);
 
-    const onValid = ({email, phone, name, avatar}: EditProfileForm) => {
-
-        return;
+    const onValid = async ({email, phone, name, avatar}: EditProfileForm) => {
         if (loading) {
             return;
         }
         if (email === '' && phone === '' && name === "") {
             setError("formErrors", {message: "Email OR phone number are required. you need to choose one."})
         }
-        editProfile({
-            email,
-            phone,
-            name
-        });
+
+        if (avatar && avatar.length > 0) {
+            const cloudflareRequest = await (await fetch(`/api/files`)).json();
+            console.log(cloudflareRequest);
+            return;
+
+            editProfile({
+                email,
+                phone,
+                name,
+                // avatarUrl: CF URL
+            });
+        } else {
+            editProfile({
+                email,
+                phone,
+                name,
+            });
+        }
+
+
     }
 
     useEffect(() => {
