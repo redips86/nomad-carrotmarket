fc1caef792fd540a6e6add253e4302440aceaac5
redips <redips@prismweaver.com>
2022-06-01 21:43:12 +0900

    [19.1] Middlewares. userRedirect

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,16 @@
 import type {NextFetchEvent, NextRequest} from "next/server";
+import {NextResponse} from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
     if (req.ua?.isBot) {
-        return new Response("Plz don't be a bot. Be human");
+        return new Response("Plz don't be a bot. Be human", {
+            status: 403
+        });
+    }
+
+    if (!req.url.includes("/api")) {
+        if (!req.cookies.carrotsession && !req.url.includes("/enter")) {
+            return NextResponse.redirect("/enter");
+        }
     }
 }
